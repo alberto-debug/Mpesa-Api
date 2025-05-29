2ccb82953249530bdacd9e846f329836a9f343fc
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 17:43:46 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/Role/RoleSeeder.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/Role/RoleSeeder.java
@@ -1,4 +1,21 @@
 package com.alberto.mpesa.api.store.domain.Role;
 
-public class RoleSeeder {
+import com.alberto.mpesa.api.store.Repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleSeeder implements CommandLineRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (roleRepository.)
+    }
 }
