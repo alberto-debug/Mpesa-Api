b5f09cc529c43f1ccfcdd94405f1fe958aef3f1f
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 18:13:28 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/infra/Security/TokenService.java
+++ src/main/java/com/alberto/mpesa/api/store/infra/Security/TokenService.java
@@ -1,10 +1,16 @@
 package com.alberto.mpesa.api.store.infra.Security;
 
 import com.alberto.mpesa.api.store.domain.model.Admin;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -13,9 +19,21 @@ public class TokenService {
     private String secret;
     private String generateToken(Admin admin){
         try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token  = JWT.create()
+                    .withIssuer("login-api")
+                    .withSubject(admin.getEmail())
+                    .withExpiresAt(this.genetateExpirationtoken())
+                    .sign(algorithm);
+            return token;
 
         }catch (JWTCreationException exception){
             throw new RuntimeException("Error while authenticating");
         }
+    }
+
+    private Instant genetateExpirationtoken(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3:00"));
     }
 }
