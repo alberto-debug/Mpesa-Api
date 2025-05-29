91861cc945b8aadfff59782d647c6cb568e1fa24
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 18:01:24 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/Repository/GuestRepository.java
@@ -0,0 +1,13 @@
+package com.alberto.mpesa.api.store.Repository;
+
+import com.alberto.mpesa.api.store.domain.model.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+
+    Optional<Guest> findBySessionId(Long id);
+}
