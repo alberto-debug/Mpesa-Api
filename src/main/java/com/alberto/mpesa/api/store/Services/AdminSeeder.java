129ab11e2c3b99589aba86b6716743315e1fe9f8
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 20:58:28 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/Services/AdminSeeder.java
@@ -0,0 +1,55 @@
+package com.alberto.mpesa.api.store.Services;
+
+
+import com.alberto.mpesa.api.store.Repository.AdminRepository;
+import com.alberto.mpesa.api.store.Repository.RoleRepository;
+import com.alberto.mpesa.api.store.domain.Role.Role;
+import com.alberto.mpesa.api.store.domain.model.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSeeder implements CommandLineRunner {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${admEmail}")
+    private String adminEmail;
+
+    @Value("${admPassword}")
+    private String adminPassword;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                .orElseGet(()-> {
+                    Role newRole = new Role();
+                    newRole.setName("ROLE_ADMIN");
+                    return roleRepository.save(newRole);
+                });
+
+
+        if (adminRepository.findByEmail(adminEmail).isEmpty()) {
+            Admin admin = new Admin();
+            admin.setName("Admin");
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
+
+            admin.getRoles().add(adminRole);
+            adminRepository.save(admin);
+
+            System.out.println("✅ Admin user created");
+        }
+    }
+}
