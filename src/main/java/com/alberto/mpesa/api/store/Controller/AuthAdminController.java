057d0eeca66879147ea96998a4735aababa2623a
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 21:27:20 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Controller/AuthAdminController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/AuthAdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/admin")
-public class AuthAdmin {
+public class AuthAdminController {
 
     @Autowired
     private AdminRepository adminRepository;
