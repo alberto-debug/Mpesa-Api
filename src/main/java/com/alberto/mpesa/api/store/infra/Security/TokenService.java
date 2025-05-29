2405768dc243789211ad82ef6763f1f0917b6c4b
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 18:18:41 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/infra/Security/TokenService.java
+++ src/main/java/com/alberto/mpesa/api/store/infra/Security/TokenService.java
@@ -4,6 +4,7 @@ import com.alberto.mpesa.api.store.domain.model.Admin;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
 
         }catch (JWTCreationException exception){
             throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    private String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+                    return JWT.require(algorithm)
+                            .withIssuer("login-api")
+                            .build()
+                            .verify(token)
+                            .getSubject();
+
+        }catch (JWTVerificationException exception){
+            throw new RuntimeException("Error Validating token");
         }
     }
 
