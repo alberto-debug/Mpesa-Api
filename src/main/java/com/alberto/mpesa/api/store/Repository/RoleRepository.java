5372d36969df97776106095625b3839b204aa412
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 17:43:44 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Repository/RoleRepository.java
+++ src/main/java/com/alberto/mpesa/api/store/Repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.alberto.mpesa.api.store.Repository;
 import com.alberto.mpesa.api.store.domain.Role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface roleRepository extends JpaRepository<Role, Long> {
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
