e83146c829f9f504d649e72b48e7d4c954698435
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 17:43:41 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Repository/AdminRepository.java
+++ src/main/java/com/alberto/mpesa/api/store/Repository/AdminRepository.java
@@ -1,9 +1,14 @@
 package com.alberto.mpesa.api.store.Repository;
 
+import com.alberto.mpesa.api.store.domain.Role.Role;
 import com.alberto.mpesa.api.store.domain.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
+    Optional<Admin> findByEmail(String email);
+    Optional<Admin> findByName(String name);
 }
