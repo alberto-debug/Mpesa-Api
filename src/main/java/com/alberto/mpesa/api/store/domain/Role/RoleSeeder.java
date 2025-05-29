78fee3edcf5059eab840b845aa4d48fffa99c19d
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 17:46:52 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/Role/RoleSeeder.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/Role/RoleSeeder.java
@@ -18,6 +18,12 @@ public class RoleSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        if (roleRepository.)
+        if (roleRepository.findByName("ROLE_GUEST").isEmpty()){
+            roleRepository.save(new Role(null, "ROLE_GUEST", new HashSet<>()));
+
+            if (roleRepository.findByName("ROLE_ADMIN").isEmpty()){
+                roleRepository.save(new Role(null,"ROLE_ADMIN", new HashSet<>()));
+            }
+        }
     }
 }
