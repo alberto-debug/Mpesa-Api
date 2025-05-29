f2e51f9bc06af1921d36407ad7f5a016ed91a922
alberto-debug <albertojunior848423803@gmail.com>
2025-05-29 21:09:20 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Controller/AuthAdmin.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/AuthAdmin.java
@@ -2,10 +2,14 @@ package com.alberto.mpesa.api.store.Controller;
 
 import com.alberto.mpesa.api.store.Repository.AdminRepository;
 import com.alberto.mpesa.api.store.Repository.RoleRepository;
+import com.alberto.mpesa.api.store.domain.model.Admin;
 import com.alberto.mpesa.api.store.infra.Security.TokenService;
 import com.nimbusds.oauth2.sdk.TokenRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +27,8 @@ public class AuthAdmin {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<Admin> adminLogin)@RequestBody
 
 }
