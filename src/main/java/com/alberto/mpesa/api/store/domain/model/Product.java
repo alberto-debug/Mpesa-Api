f60c6acf5be68acad9456c360efe2e284bb04b71
alberto-debug <albertojunior848423803@gmail.com>
2025-06-04 21:45:11 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Product.java
@@ -0,0 +1,19 @@
+package com.alberto.mpesa.api.store.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
