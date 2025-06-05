b49eda077b987c79806084226b7e73caa7e5bdc5
alberto-debug <albertojunior848423803@gmail.com>
2025-06-05 11:27:32 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
@@ -19,11 +19,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany
-    @JoinTable(
-            name = "productId"
-    )
-    private Set<Cart> cart  = new  HashSet<>();
-
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem>  cartItems= new HashSet<>();
 
 }
---
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/CartItem.java
@@ -0,0 +1,4 @@
+package com.alberto.mpesa.api.store.domain.model;
+
+public class CartItem {
+}
