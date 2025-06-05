199e4227c82c20095908413a92a483110ab11bbb
alberto-debug <albertojunior848423803@gmail.com>
2025-06-05 11:33:02 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/model/CartItem.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/CartItem.java
@@ -1,4 +1,30 @@
 package com.alberto.mpesa.api.store.domain.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
 public class CartItem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
