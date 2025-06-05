5cd97288729abaaad4d999f96c6b35125490bc72
alberto-debug <albertojunior848423803@gmail.com>
2025-06-05 20:11:06 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/Controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.alberto.mpesa.api.store.Controller;
+
+import com.alberto.mpesa.api.store.DTO.ProductRequestDTO;
+import com.alberto.mpesa.api.store.DTO.ProductResponseDTO;
+import com.alberto.mpesa.api.store.Services.ProductService;
+import com.alberto.mpesa.api.store.domain.model.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@AllArgsConstructor
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+
+    @PostMapping("/createProduct")
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO dto){
+
+        ProductResponseDTO created  = productService.createProduct(dto);
+        return ResponseEntity.ok(created);
+    }
+
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts(){
+        return ResponseEntity.ok(productService.findAllProducts());
+    }
+
+
+}
