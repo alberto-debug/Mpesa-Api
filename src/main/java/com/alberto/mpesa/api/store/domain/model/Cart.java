97d5965099501385afa0d6482482c574f0bce971
alberto-debug <albertojunior848423803@gmail.com>
2025-06-05 11:52:37 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
@@ -20,6 +20,5 @@ public class Cart {
     private Long id;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CartItem>  cartItems= new HashSet<>();
-
+    private Set<CartItem> cartItems = new HashSet<>();
 }
