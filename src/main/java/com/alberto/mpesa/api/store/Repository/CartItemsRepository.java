565b461ba9ec2066bcd0c4780def01e8145062d1
alberto-debug <albertojunior848423803@gmail.com>
2025-06-05 14:01:31 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/Repository/CartItemsRepository.java
@@ -0,0 +1,10 @@
+package com.alberto.mpesa.api.store.Repository;
+
+
+import com.alberto.mpesa.api.store.domain.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemsRepository extends JpaRepository<CartItem, Long> {
+}
