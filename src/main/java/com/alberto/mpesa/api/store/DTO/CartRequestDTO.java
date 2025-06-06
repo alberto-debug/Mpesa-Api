b62b42fd33963e32cf0063478282c903cea43c16
alberto-debug <albertojunior848423803@gmail.com>
2025-06-06 10:58:53 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/DTO/CartRequestDTO.java
@@ -0,0 +1,18 @@
+package com.alberto.mpesa.api.store.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartRequestDTO {
+
+    private List<CartItemDetailDTO> list;
+}
