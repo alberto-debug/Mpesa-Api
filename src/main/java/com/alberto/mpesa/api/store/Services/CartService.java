91855724bb48c307c4b3f5d9d95ca098d25682c7
alberto-debug <albertojunior848423803@gmail.com>
2025-06-06 12:07:03 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
@@ -1,11 +1,27 @@
 package com.alberto.mpesa.api.store.Services;
 
+import com.alberto.mpesa.api.store.DTO.CartItemDTO;
+import com.alberto.mpesa.api.store.DTO.CartRequestDTO;
+import com.alberto.mpesa.api.store.DTO.CartResponseDTO;
+import com.alberto.mpesa.api.store.DTO.ResponseDTO;
 import com.alberto.mpesa.api.store.Repository.CartRepository;
 import com.alberto.mpesa.api.store.Repository.ProductRepository;
+import com.alberto.mpesa.api.store.domain.model.Cart;
+import com.alberto.mpesa.api.store.domain.model.CartItem;
+import com.alberto.mpesa.api.store.domain.model.Product;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartService {
 
     @Autowired
@@ -13,6 +29,24 @@ public class CartService {
 
     @Autowired
     private ProductRepository productRepository;
+
+
+    // 📌 Create a new cart with optional items
+    @Transactional
+    public CartResponseDTO createCart(CartRequestDTO cartRequestDTO){
+
+        Cart cart =  new Cart();
+
+        List<CartItem>  cartItems = cartRequestDTO.getItems().stream().map(dto ->{
+            Product product = productRepository.findById(dto.getProductId())
+                    .orElseThrow(() -> new IllegalArgumentException("Product not found with id: " + dto.getProductId()));
+
+            CartItem item = new CartItem();
+            item.setProduct(product);
+            item.setCart(dto.getCart());
+            item.setQuantity(dto.getQuantity());
+        }).collect(Collectors.toList());
+    }
 
 
 
