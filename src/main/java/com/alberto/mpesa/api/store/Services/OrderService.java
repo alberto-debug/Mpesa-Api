7a2e7e793325b5199ed5e8dfd257a75d16e3ec0d
alberto-debug <albertojunior848423803@gmail.com>
2025-06-23 03:55:51 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Services/OrderService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/OrderService.java
@@ -1,10 +1,19 @@
 package com.alberto.mpesa.api.store.Services;
 
 
+import com.alberto.mpesa.api.store.Repository.IOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private IOrderRepository iOrderRepository;
+
+    @Autowired
+    private CartService cartService;
+
 
 
 }
