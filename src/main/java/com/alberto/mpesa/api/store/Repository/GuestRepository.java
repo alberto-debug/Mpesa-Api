268345a17b3545da3ffa3bba830d5551f73098ba
alberto-debug <albertojunior848423803@gmail.com>
2025-06-28 22:51:15 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Repository/GuestRepository.java
+++ src/main/java/com/alberto/mpesa/api/store/Repository/GuestRepository.java
@@ -1,6 +1,5 @@
 package com.alberto.mpesa.api.store.Repository;
 
-import com.alberto.mpesa.api.store.domain.model.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
