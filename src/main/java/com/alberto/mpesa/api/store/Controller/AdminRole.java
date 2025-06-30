7910d2f29cf397fef1b61230db349358a493904b
alberto-debug <albertojunior848423803@gmail.com>
2025-06-30 18:50:51 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Controller/AdminRole.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/AdminRole.java
@@ -1,7 +1,30 @@
 package com.alberto.mpesa.api.store.Controller;
 
+import com.alberto.mpesa.api.store.Repository.AdminRepository;
+import com.alberto.mpesa.api.store.Repository.RoleRepository;
+import com.alberto.mpesa.api.store.infra.Security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin/managers")
 public class AdminRole {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
+
 }
