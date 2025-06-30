e2cedc6f5d3f2d4b39e5ea317cda068f84c62b76
alberto-debug <albertojunior848423803@gmail.com>
2025-06-30 19:17:16 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/Controller/AdminRole.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/AdminRole.java
@@ -1,12 +1,14 @@
 package com.alberto.mpesa.api.store.Controller;
 
+import com.alberto.mpesa.api.store.DTO.ManagerCreationDTO;
 import com.alberto.mpesa.api.store.Repository.AdminRepository;
 import com.alberto.mpesa.api.store.Repository.RoleRepository;
+import com.alberto.mpesa.api.store.domain.model.Admin;
 import com.alberto.mpesa.api.store.infra.Security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/managers")
@@ -23,6 +25,18 @@ public class AdminRole {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createManager(@RequestBody ManagerCreationDTO body,
+                                           @RequestHeader("Authorization") String token){
+
+        String adminEmail = tokenService.getEmailFromToken(token.replace("Bearer ", " "));
+        Admin admin = adminRepository.findByEmail(adminEmail)
+                .orElseThrow(()-> new RuntimeException("Admin not found"));
+
+
+        boolean isAdmin = admin.getRoles().stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN"));
+    }
 
 
 
