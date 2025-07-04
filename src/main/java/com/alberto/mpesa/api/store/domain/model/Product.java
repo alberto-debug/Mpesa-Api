5f2238a5c3ee43cf3fb48ed123cd39635929a832
alberto-debug <albertojunior848423803@gmail.com>
2025-07-04 16:31:55 +0300

    changes

---
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Product.java
@@ -0,0 +1,42 @@
+package com.alberto.mpesa.api.store.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "discount_id")
+    private Discount discount;
+
+    private int stockQuantity;
+
+    private LocalDate expiryDate;
+
+    private String imageUrl;
+}
