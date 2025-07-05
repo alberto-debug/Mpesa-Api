ff0dfa2c3cab52bcc5fed47673111a1b47d615e8
alberto-debug <albertojunior848423803@gmail.com>
2025-07-05 21:26:46 +0300

    Changes

--- src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
@@ -1,5 +1,20 @@
 package com.alberto.mpesa.api.store.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Table(name = "cart")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Product product;
 }
