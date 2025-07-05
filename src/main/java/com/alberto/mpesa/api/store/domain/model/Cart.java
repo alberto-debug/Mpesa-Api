c5e96608126948a3f8b7432b970b1c35cd0ec844
alberto-debug <albertojunior848423803@gmail.com>
2025-07-05 22:28:16 +0300

    changes

--- src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
-@Table(name = "cart")
+@Table(name = "carts")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,4 +20,7 @@ public class Cart {
     private Long id;
 
     private Product product;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItems = new HashSet<>();
 }
