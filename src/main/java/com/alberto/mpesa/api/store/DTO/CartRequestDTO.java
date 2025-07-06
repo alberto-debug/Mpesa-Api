2cbb2148a1ba4c3028c32f49849789bc272513e9
alberto-debug <albertojunior848423803@gmail.com>
2025-07-06 12:55:05 +0300

    feature/Cart

--- src/main/java/com/alberto/mpesa/api/store/DTO/CartRequestDTO.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/CartRequestDTO.java
@@ -1,4 +1,16 @@
 package com.alberto.mpesa.api.store.DTO;
 
-public record CartRequestDTO() {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartRequestDTO {
+
+    private List<CartItemDTO> items;
 }
