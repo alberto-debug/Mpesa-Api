534a9f841b7fb5d2218ee6fb83c2b497775034ef
alberto-debug <albertojunior848423803@gmail.com>
2025-07-09 14:29:54 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponse.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponse.java
@@ -4,9 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponse {
 
+    private Long id;
+    private String productName;
+    private BigDecimal price;
 }
