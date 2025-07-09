97c1888c2eb8f1b0e222a7bd4a64fe1406563487
alberto-debug <albertojunior848423803@gmail.com>
2025-07-09 14:27:21 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/DTO/ProductRequest.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/ProductRequest.java
@@ -1,4 +1,11 @@
 package com.alberto.mpesa.api.store.DTO;
 
+import java.math.BigDecimal;
+
 public class ProductRequest {
+
+    private Long productId;
+    private Long productName;
+    private int quantity;
+    private BigDecimal price;
 }
