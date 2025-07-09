094a8014116560b39cd0b31be08fe4529df8221e
alberto-debug <albertojunior848423803@gmail.com>
2025-07-09 14:27:24 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponse.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponse.java
@@ -1,4 +1,12 @@
 package com.alberto.mpesa.api.store.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
+
 }
