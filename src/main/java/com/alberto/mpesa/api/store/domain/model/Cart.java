a1242a07f9b41d7df5ce348df5779bbf8159c851
alberto-debug <albertojunior848423803@gmail.com>
2025-07-10 16:19:45 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
+++ src/main/java/com/alberto/mpesa/api/store/domain/model/Cart.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,11 +22,6 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //Many carts can belong to one customer.
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "customer_id")
-    private Customer customer;
-
     //One cart can contain many cart items. get list of all cart items
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> cartItems = new HashSet<>();
@@ -33,5 +29,8 @@ public class Cart {
     @Enumerated(EnumType.STRING)
     private CartStatus status; // ACTIVE, CHECKED_OUT, CANCELLED
 
+    private BigDecimal total;
+
     private LocalDateTime createdAt = LocalDateTime.now();
+
 }
