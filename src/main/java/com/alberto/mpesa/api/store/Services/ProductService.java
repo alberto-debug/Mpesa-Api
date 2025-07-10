da08d3577b40ee936b7380288367bea5398bbcbc
alberto-debug <albertojunior848423803@gmail.com>
2025-07-11 02:49:39 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
@@ -1,4 +1,12 @@
 package com.alberto.mpesa.api.store.Services;
 
+import com.alberto.mpesa.api.store.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    
 }
