38a0aa496e84616b9005d9d1bdabf6936b3b4c30
alberto-debug <albertojunior848423803@gmail.com>
2025-07-10 16:03:55 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/Repository/CustomerRepository.java
+++ src/main/java/com/alberto/mpesa/api/store/Repository/CustomerRepository.java
@@ -4,5 +4,6 @@ import com.alberto.mpesa.api.store.domain.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
