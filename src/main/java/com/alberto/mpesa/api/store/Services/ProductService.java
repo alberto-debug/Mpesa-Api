1d8a0eca263a59ec3377ddceddce46efc0b781df
alberto-debug <albertojunior848423803@gmail.com>
2025-07-11 02:51:30 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
@@ -1,12 +1,20 @@
 package com.alberto.mpesa.api.store.Services;
 
+import com.alberto.mpesa.api.store.DTO.ProductRequestDTO;
+import com.alberto.mpesa.api.store.DTO.ProductResponseDTO;
 import com.alberto.mpesa.api.store.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
 
-    
+
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequestDTO){
+
+
+    }
 }
