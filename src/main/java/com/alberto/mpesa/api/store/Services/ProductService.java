5299f6f8ed4100d3ffaf9b62b7f2e3cc93e73081
alberto-debug <albertojunior848423803@gmail.com>
2025-07-11 03:12:02 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/ProductService.java
@@ -3,17 +3,28 @@ package com.alberto.mpesa.api.store.Services;
 import com.alberto.mpesa.api.store.DTO.ProductRequestDTO;
 import com.alberto.mpesa.api.store.DTO.ProductResponseDTO;
 import com.alberto.mpesa.api.store.Repository.ProductRepository;
+import com.alberto.mpesa.api.store.domain.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
 
+    public ProductResponseDTO createProduct(ProductRequestDTO dto){
 
-    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequestDTO){
+        Product product = new Product();
+        product.setName(dto.getProductName());
+        product.setStockQuantity(dto.getQuantity());
+        product.setPrice(dto.getPrice());
 
         return null;
     }
