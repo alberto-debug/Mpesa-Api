b85ba147e6788201ff4991ecc6918e86e8098920
alberto-debug <albertojunior848423803@gmail.com>
2025-07-11 03:11:57 +0300

    feature/Product

--- src/main/java/com/alberto/mpesa/api/store/DTO/ProductRequestDTO.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/ProductRequestDTO.java
@@ -1,9 +1,13 @@
 package com.alberto.mpesa.api.store.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class ProductRequestDTO {
-
 
     private String productName;
     private int quantity;
