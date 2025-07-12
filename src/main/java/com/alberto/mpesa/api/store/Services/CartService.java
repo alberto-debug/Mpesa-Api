cffa6d63cf05fe4fa3a7f80be1256171e94bc30e
alberto-debug <albertojunior848423803@gmail.com>
2025-07-12 21:50:47 +0300

    feature/Products

--- src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
@@ -5,9 +5,16 @@ import com.alberto.mpesa.api.store.DTO.CartRequestDTO;
 import com.alberto.mpesa.api.store.DTO.CartResponseDTO;
 import com.alberto.mpesa.api.store.Repository.CartRepository;
 import com.alberto.mpesa.api.store.Repository.ProductRepository;
+import com.alberto.mpesa.api.store.domain.Enums.CartStatus;
+import com.alberto.mpesa.api.store.domain.model.Cart;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @Service
 @AllArgsConstructor
@@ -16,7 +23,14 @@ public class CartService {
     private final CartRepository cartRepository;
     private final ProductRepository productRepository;
 
-    public CartResponseDTO createProduct(CartRequestDTO cartRequestDTO){
+    public CartResponseDTO createProduct(CartRequestDTO cartRequest){
+
+        Cart cart = new Cart();
+        cart.setStatus(CartStatus.ACTIVE);
+        cart.setCreatedAt(LocalDateTime.now());
+        cart.setCartItems(new HashSet<>());
+        cart.setTotal(BigDecimal.ZERO);
+
 
         return  null;
 
