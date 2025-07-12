0840bef291e2997c56effddf80ae2f390dca98f9
alberto-debug <albertojunior848423803@gmail.com>
2025-07-12 21:46:52 +0300

    feature/Products

--- src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
@@ -1,13 +1,25 @@
 package com.alberto.mpesa.api.store.Services;
 
 
+import com.alberto.mpesa.api.store.DTO.CartRequestDTO;
+import com.alberto.mpesa.api.store.DTO.CartResponseDTO;
 import com.alberto.mpesa.api.store.Repository.CartRepository;
+import com.alberto.mpesa.api.store.Repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CartService {
 
-    @Autowired
-    private CartRepository cartRepository;
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+
+    public CartResponseDTO createProduct(CartRequestDTO cartRequestDTO){
+
+        return  null;
+
+    }
+
 }
