3b5ee5af8942c5c1921ca2bca88338a3d699f71b
alberto-debug <albertojunior848423803@gmail.com>
2025-07-20 17:36:19 +0300

    feature/Cart

--- src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
+++ src/main/java/com/alberto/mpesa/api/store/Services/CartService.java
@@ -67,8 +67,7 @@ public class CartService {
         cart = cartRepository.save(cart);
         return mapToResponse(cart);
     }
-
-
+    
     @Transactional
     public CartResponseDTO updateQuantity(Long cartId, Long productId, int quantity){
         Cart cart = cartRepository.findById(cartId)
