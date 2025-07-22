ec3ba24746ac31b4308af8d1a48893a1049ef1b6
alberto-debug <albertojunior848423803@gmail.com>
2025-07-23 02:02:38 +0300

    feature/Cart

---
+++ src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
@@ -0,0 +1,17 @@
+package com.alberto.mpesa.api.store.Controller;
+
+import com.alberto.mpesa.api.store.Services.CartService;
+import com.alberto.mpesa.api.store.Services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+@AllArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+    private final ProductService productService;
+
+}
