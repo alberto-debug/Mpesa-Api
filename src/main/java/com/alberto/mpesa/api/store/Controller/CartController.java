63d9f36a34ea22f0163705780b0e88f3983fdb65
alberto-debug <albertojunior848423803@gmail.com>
2025-07-23 02:05:54 +0300

    feature/Cart

--- src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/api/carts")
 @AllArgsConstructor
 public class CartController {
 
