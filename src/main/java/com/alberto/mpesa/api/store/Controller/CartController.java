f5c105540fbc0d7313e85295ba5f3e37cde080ad
alberto-debug <albertojunior848423803@gmail.com>
2025-07-23 04:27:41 +0300

    feature/Cart

--- src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
@@ -1,8 +1,14 @@
 package com.alberto.mpesa.api.store.Controller;
 
+import com.alberto.mpesa.api.store.DTO.CartRequestDTO;
+import com.alberto.mpesa.api.store.DTO.CartResponseDTO;
 import com.alberto.mpesa.api.store.Services.CartService;
 import com.alberto.mpesa.api.store.Services.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,11 @@ public class CartController {
 
     private final CartService cartService;
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<CartResponseDTO> addTOCart(@RequestBody CartRequestDTO cartRequest){
+        CartResponseDTO response = cartService.addToCart(cartRequest);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
 
 }
