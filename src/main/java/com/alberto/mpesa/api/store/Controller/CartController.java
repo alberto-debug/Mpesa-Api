f613ab6f7e7bf9b683c6256659aa1eb8b3b3ef1b
alberto-debug <albertojunior848423803@gmail.com>
2025-07-26 11:43:42 +0300

    feature/Cart

--- src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/CartController.java
@@ -2,6 +2,7 @@ package com.alberto.mpesa.api.store.Controller;
 
 import com.alberto.mpesa.api.store.DTO.CartRequestDTO;
 import com.alberto.mpesa.api.store.DTO.CartResponseDTO;
+import com.alberto.mpesa.api.store.Repository.CartRepository;
 import com.alberto.mpesa.api.store.Services.CartService;
 import com.alberto.mpesa.api.store.Services.ProductService;
 import lombok.AllArgsConstructor;
@@ -17,12 +18,11 @@ public class CartController {
     private final CartService cartService;
     private final ProductService productService;
 
-    @PostMapping("/add")
-    public ResponseEntity<CartResponseDTO> addTOCart(@RequestBody CartRequestDTO cartRequest){
-        CartResponseDTO response = cartService.addToCart(cartRequest);
-        return new ResponseEntity<>(response,HttpStatus.CREATED);
-    }
-
+//    @PostMapping("/add")
+//    public ResponseEntity<CartResponseDTO> addTOCart(@RequestBody CartRequestDTO cartRequest){
+//        CartResponseDTO response = cartService.addToCart(cartRequest);
+//        return new ResponseEntity<>(response,HttpStatus.CREATED);
+//    }
 
     @DeleteMapping("/{cartId}/item/{productId}")
     public ResponseEntity<CartResponseDTO> removeFromCart(
@@ -32,6 +32,12 @@ public class CartController {
         CartResponseDTO response = cartService.removeFromCart(cartId, productId);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/add")
+    private ResponseEntity<CartResponseDTO> addToCart(@RequestBody CartRequestDTO cartRequestDTO){
+        CartResponseDTO responseDTO = cartService.addToCart(cartRequestDTO);
+
     }
 
 }
