1b61540f3707c803e1b4b07840d2fb89fb5e19ca
alberto-debug <albertojunior848423803@gmail.com>
2025-07-27 23:43:59 +0300

    Cart Controller

--- src/main/java/com/alberto/mpesa/api/store/Controller/ManagerController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/ManagerController.java
@@ -6,6 +6,7 @@ import com.alberto.mpesa.api.store.Repository.AdminRepository;
 import com.alberto.mpesa.api.store.domain.model.Admin;
 import com.alberto.mpesa.api.store.infra.Security.TokenService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/login")
 @AllArgsConstructor
@@ -40,6 +42,9 @@ public class ManagerController {
             return ResponseEntity.status(403).body(new ResponseDTO("Access denied", null));
 
         }
+
+        String token = tokenService.generateToken(manager);
+        log.info("Manager logged with email: {} " , manager.getEmail());
 
         return null;
     }
