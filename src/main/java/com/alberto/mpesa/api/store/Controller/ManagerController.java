4f2a99dae1b2dd457454c7e010c2fafad70ed288
alberto-debug <albertojunior848423803@gmail.com>
2025-07-27 23:46:58 +0300

    manager controller

--- src/main/java/com/alberto/mpesa/api/store/Controller/ManagerController.java
+++ src/main/java/com/alberto/mpesa/api/store/Controller/ManagerController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 @Slf4j
 @RestController
 @RequestMapping("/login")
