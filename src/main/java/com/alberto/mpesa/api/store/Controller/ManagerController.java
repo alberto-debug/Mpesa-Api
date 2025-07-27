d2d2994bd1dccab99fd5ca2b9026c418f9cf0294
alberto-debug <albertojunior848423803@gmail.com>
2025-07-27 23:42:20 +0300

    Cart Controller

---
+++ src/main/java/com/alberto/mpesa/api/store/Controller/ManagerController.java
@@ -0,0 +1,46 @@
+package com.alberto.mpesa.api.store.Controller;
+
+import com.alberto.mpesa.api.store.DTO.LoginRequestDTO;
+import com.alberto.mpesa.api.store.DTO.ResponseDTO;
+import com.alberto.mpesa.api.store.Repository.AdminRepository;
+import com.alberto.mpesa.api.store.domain.model.Admin;
+import com.alberto.mpesa.api.store.infra.Security.TokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@AllArgsConstructor
+public class ManagerController {
+
+    private final AdminRepository adminRepository;
+    private final TokenService tokenService;
+    private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> managerLogin(@RequestBody LoginRequestDTO body){
+
+        Admin manager =  adminRepository.findByEmail(body.email())
+                .orElseThrow(()->new RuntimeException("Manager not found"));
+
+        if (!passwordEncoder.matches(body.password(), manager.getPassword())){
+            return ResponseEntity.badRequest().body(new ResponseDTO("Invalid Credentials", null));
+        }
+
+        boolean isManager = manager.getRoles().stream()
+                .anyMatch(role -> role.getName().equals("ROLE_MANAGER"));
+
+        if (isManager){
+            return ResponseEntity.status(403).body(new ResponseDTO("Access denied", null));
+
+        }
+
+        return null;
+    }
+}
