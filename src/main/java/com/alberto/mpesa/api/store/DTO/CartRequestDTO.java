2890ee3c21ef5463b32e794b2feb7a106cbf3b07
alberto-debug <albertojunior848423803@gmail.com>
2025-07-29 16:29:58 +0300

    manager controller

--- src/main/java/com/alberto/mpesa/api/store/DTO/CartRequestDTO.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/CartRequestDTO.java
@@ -1,20 +1,14 @@
 package com.alberto.mpesa.api.store.DTO;
 
-
-import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.lang.NonNull;
 
 import java.util.List;
 
-//Incoming cart creation/update
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartRequestDTO {
-
-    private CustomerDTO customer;
     private List<CartItemDTO> items;
 }
