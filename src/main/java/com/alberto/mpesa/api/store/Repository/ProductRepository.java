3592900b855ba51e1af390a484122d6ea7e5706e
alberto-debug <albertojunior848423803@gmail.com>
2025-07-29 16:30:55 +0300

    manager controller

--- src/main/java/com/alberto/mpesa/api/store/Repository/ProductRepository.java
+++ src/main/java/com/alberto/mpesa/api/store/Repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.alberto.mpesa.api.store.Repository;
 
+import com.alberto.mpesa.api.store.domain.model.Category;
 import com.alberto.mpesa.api.store.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long > {
+
 }
