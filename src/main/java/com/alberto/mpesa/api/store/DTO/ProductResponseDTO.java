6786fc62178a90c87b1f2c4d5224ed8e479b19af
alberto-debug <albertojunior848423803@gmail.com>
2025-07-29 16:31:04 +0300

    manager controller

--- src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponseDTO.java
+++ src/main/java/com/alberto/mpesa/api/store/DTO/ProductResponseDTO.java
@@ -1,16 +1,23 @@
 package com.alberto.mpesa.api.store.DTO;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponseDTO {
-
     private Long id;
     private String productName;
+    private int quantity;
+    private String category;
     private BigDecimal price;
+    private LocalDate expiryDate;
+    private String imageUrl;
 }
